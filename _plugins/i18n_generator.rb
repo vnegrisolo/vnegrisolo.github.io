b527e7853922558b7ea8923d98e217ddad9507f0
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-07-11 13:17:03 -0300

    Create i18n plugin to duplicate pages across locales

---
+++ _plugins/i18n_generator.rb
@@ -0,0 +1,34 @@
+module Jekyll
+  class I18nGenerator < Generator
+    safe true
+
+    def generate(site)
+      @site = site
+      add_pages(new_i18n_pages)
+    end
+
+    private
+
+    def new_i18n_pages
+      pages.map do |page|
+        languages.map do |language|
+          new_page = page.clone
+          new_page.dir = "/#{language}#{page.dir}"
+          new_page
+        end
+      end.flatten
+    end
+
+    def add_pages(pages)
+      pages.each { |page| @site.pages << page }
+    end
+
+    def languages
+      @languages ||= @site.config['languages'][1..-1]
+    end
+
+    def pages
+      @pages ||= @site.pages.select { |page| page.name.end_with?('.html') }
+    end
+  end
+end
