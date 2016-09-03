1b44d0033d434d2d3d1d1e6111ce6771754f17b2
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-09-03 12:12:26 -0400

    Change social email by gist

--- js/syntax-highlight.js
+++ js/syntax-highlight.js
@@ -13,18 +13,3 @@ $('div.highlighter-rouge').map(function(){
   }
   codeSnippet.prepend(header);
 });
-
-// classes = $.unique($('.highlighter-rouge pre code span').map(function() { return $(this).attr('class'); }).sort());
-// languages = $.unique($('.highlighter-rouge .header').map(function(){ return $(this).data('language'); }).sort());
-// classes.each(function() {
-//   css = this;
-//   languages.each(function(){
-//     lang = this;
-//     color = 'background: #073642; color: '+$('.highlighter-rouge.language-'+lang+' pre code span.'+css).css('color');
-//     texts = $.unique($('.highlighter-rouge.language-'+lang+' pre code span.'+css).map(function(){ return $(this).text(); }).sort());
-//     texts.each(function(){
-//       console.log("%c "+css + " => " + lang + " => "+this, color);
-//     });
-//   });
-// });
-
