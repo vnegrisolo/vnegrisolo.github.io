9af484b4b89985954acd21e749bbd0b9d4497171
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-09-21 21:49:57 -0400

    Make code snippets file path copyable

--- js/syntax-highlight.js
+++ js/syntax-highlight.js
@@ -2,9 +2,8 @@ SyntaxHighlight = {
   buildHeader: function(lang, path){
     return $('<div/>', {
       'class': 'header small',
-      'data-lang': lang,
-      'data-path': path
-    });
+      'data-lang': lang
+    }).prepend(path || '&nbsp;');
   }
 }
 
