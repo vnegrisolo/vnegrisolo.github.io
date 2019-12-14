73914d293b56bffa26919d1652fe4f93fd48a6ba
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2019-12-14 12:56:46 -0500

    💄  Include svgs for fill manipulation

--- js/bookmark-link.js
+++ js/bookmark-link.js
@@ -1,7 +1,7 @@
 document.querySelectorAll("h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]").forEach(header => {
   var icon = document.createElement("img");
   icon.setAttribute("class", "icon");
-  icon.setAttribute("src", "/svgs/link.svg");
+  icon.setAttribute("src", "/svgs/link-gray-dark.svg");
   icon.setAttribute("alt", "Link");
 
   var link = document.createElement("a");
