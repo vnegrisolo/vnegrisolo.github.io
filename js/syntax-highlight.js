3fc9bcdc063b15bcbb9aeea21c4bd8334639cbab
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2019-12-14 18:41:45 -0500

    🎨  Inline bookmark svg

--- js/syntax-highlight.js
+++ js/syntax-highlight.js
@@ -1,9 +1,8 @@
 document.querySelectorAll("div.highlighter-rouge").forEach(el => {
-  var lang = el.getAttribute("class").match(/language-(\S+)/);
-  var title = el.dataset.path;
-  console.warn({lang, title});
+  const lang = el.getAttribute("class").match(/language-(\S+)/);
+  const title = el.dataset.path;
 
-  var header = document.createElement("div");
+  const header = document.createElement("div");
   header.setAttribute("class", "header");
   header.setAttribute("data-lang", lang && lang[1] || "");
   header.innerHTML = title || "&nbsp;";
