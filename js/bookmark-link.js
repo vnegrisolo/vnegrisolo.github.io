ebc50a30d290cec618447d5de41021cfae73ea4e
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2020-01-10 21:15:32 -0500

    🎨  Change bookmark link to work only for posts

--- js/bookmark-link.js
+++ js/bookmark-link.js
@@ -1,7 +1,9 @@
-document.querySelectorAll("h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]").forEach(header => {
-  const link = document.createElement("a");
-  link.setAttribute("class", "bookmark");
-  link.setAttribute("href", `#${header.getAttribute("id")}`);
+document.querySelectorAll("article.post").forEach(post => {
+  post.querySelectorAll("h1[id],h2[id],h3[id],h4[id]").forEach(header => {
+    const link = document.createElement("a");
+    link.setAttribute("class", "bookmark");
+    link.setAttribute("href", `#${header.getAttribute("id")}`);
 
-  header.prepend(link);
+    header.prepend(link);
+  });
 });
